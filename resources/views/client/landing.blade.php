93734fe78fb7e1e6d119a1beaeaf07e1d6939b3d
constationer <dimasputrap26@gmail.com>
2021-01-20 12:30:08 +0700

    baru lagi rik

--- resources/views/client/landing.blade.php
+++ resources/views/client/landing.blade.php
@@ -36,49 +36,11 @@
                                 <img src="{{asset('client/img/naskah.png')}}" alt="">
                             </div>
                             <div class="col-md-6 col-12 d-flex align-items-center text-justify">
-                                <p> Digital Publishing System adalah sebuah portal berbasis web yang bertujuan untuk menjembatani penerbit-penerbit Kompas Gramedia dengan pengarang. Dengan adanya Digital Publishing System, sistem penerimaan dan pengelolaan naskah akan menjadi lebih terorganisir. Selain itu, pengarang juga lebih mudah untuk menyalurkan naskahnya ke penerbit.</p>
+                                <p> kirimnaskah.mizan.com adalah sebuah portal website yang ditujukan bagi para penulis dan calon penulis untuk mengirim naskah ke penerbit-penerbit yang ada di Mizan Grup dengan lebih mudah dan terorganisir.</p>
                             </div>
                         </div>
                     </div>
                 </div>
-            </div>
-        </div>
-    </div>
-</section>
-<section class="doc_feedback_area parallaxie" data-background="{{asset('client/img/bg.jpg')}}" style="background: url({{asset('client/img/home_one/feedback_bg.jpg')}}) no-repeat scroll;">
-    <div class="overlay_bg"></div>
-    <div class="container">
-        <div class="doc_feedback_info">
-            <div class="doc_feedback_slider">
-                <div class="item">
-                    <div class="author_img">
-                        <img src="{{asset('client/img/home_one/icon/feedback.jpg')}}" alt="">
-                    </div>
-                    <p>
-                        Nulla porttitor accumsan tincidunt. Praesent sapien massa, convallis a pellentesque nec, egestas non nisi. Nulla quis lorem ut libero malesuada feugiat. Vivamus suscipit tortor eget felis porttitor volutpat. Proin eget tortor risus.
-                    </p>
-                    <h5>Rodney Artichoke</h5>
-                    <h6>UI/UX designer</h6>
-                </div>
-                <div class="item">
-                    <div class="author_img">
-                        <img src="{{asset('client/img/home_one/icon/feedback.jpg')}}" alt="">
-                    </div>
-                    <p>
-                        Nulla porttitor accumsan tincidunt. Praesent sapien massa, convallis a pellentesque nec, egestas non nisi. Nulla quis lorem ut libero malesuada feugiat. Vivamus suscipit tortor eget felis porttitor volutpat. Proin eget tortor risus.
-                    </p>
-                    <h5>Rodney Artichoke</h5>
-                    <h6>UI/UX designer</h6>
-                </div>
-            </div>
-            <div class="slider_nav">
-                <div class="prev">
-                    <span class="arrow"></span>
-                </div>
-                <div class="next">
-                    <span class="arrow"></span>
-                </div>
-
             </div>
         </div>
     </div>
@@ -126,12 +88,12 @@
         <div class="row align-items-center">
             <div class="col-lg-9 col-sm-8">
                 <div class="action_text">
-                    <h2>Con't find an answer?</h2>
-                    <p>Head over to our Discord channel</p>
+                    <h2>Belum memiliki akun?</h2>
+                    <p>Daftarkan dan ajukan naskah mu disini.</p>
                 </div>
             </div>
             <div class="col-lg-3 col-sm-4">
-                <a href="#" class="action_btn">Discord Channel <i class="arrow_right"></i></a>
+                <a href="{{ route('register') }}" class="action_btn">Daftar <i class="arrow_right"></i></a>
             </div>
         </div>
     </div>
--- resources/views/client/layouts/footer.blade.php
+++ resources/views/client/layouts/footer.blade.php
@@ -9,49 +9,35 @@
                 <div class="col-lg-4 col-sm-6">
                     <div class="f_widget subscribe_widget">
                         <a href="index.html" class="f_logo"><img src="{{asset('client/img/logo.png')}}" alt=""></a>
-                        <p>Digital Publishing System adalah sebuah portal berbasis web yang bertujuan untuk menjembatani penerbit-penerbit Kompas Gramedia dengan pengarang. Dengan adanya Digital Publishing System, sistem penerimaan dan pengelolaan naskah akan menjadi lebih terorganisir. Selain itu, pengarang juga lebih mudah untuk menyalurkan naskahnya ke penerbit..</p>
+                        <p>kirimnaskah.mizan.com adalah sebuah portal website yang ditujukan bagi para penulis dan calon penulis untuk mengirim naskah ke penerbit-penerbit yang ada di Mizan Grup dengan lebih mudah dan terorganisir.</p>
                     </div>
                 </div>
-                <div class="col-lg-3 col-sm-6">
-                    <div class="f_widget link_widget pl_30">
-                        <h3 class="f_title">Company</h3>
-                        <ul class="list-unstyled link_list">
-                            <li><a href="#">About Us</a></li>
-                            <li><a href="#">Testimonials</a></li>
-                            <li><a href="#">Affiliates</a></li>
-                            <li><a href="#">Partners</a></li>
-                            <li><a href="#">Careers</a></li>
-                            <li><a href="#">Docly for Good</a></li>
-                            <li><a href="#">Contact Us</a></li>
-                        </ul>
-                    </div>
-                </div>
-                <div class="col-lg-2 col-sm-6">
-                    <div class="f_widget link_widget">
-                        <h3 class="f_title">Support</h3>
-                        <ul class="list-unstyled link_list">
-                            <li><a href="index-3.html">Help Desk</a></li>
-                            <li><a href="#">Knowledge Base</a></li>
-                            <li><a href="#">Live Chat</a></li>
-                            <li><a href="#">Integrations</a></li>
-                            <li><a href="#">Reports</a></li>
-                            <li><a href="#">iOS & Android</a></li>
-                            <li><a href="#">Messages</a></li>
-                        </ul>
-                    </div>
-                </div>
-                <div class="col-lg-3 col-sm-6">
+                <div class="col-lg-5 col-sm-6">
                     <div class="f_widget link_widget pl_70">
-                        <h3 class="f_title">Doc Pages</h3>
+                        <h3 class="f_title">Head Office</h3>
                         <ul class="list-unstyled link_list">
-                            <li><a href="doclist.html">Doc Topic</a></li>
-                            <li><a href="#">Free Training</a></li>
-                            <li><a href="doc-archive.html">Doc Archive</a></li>
-                            <li><a href="changelog.html">Changelog</a></li>
-                            <li><a href="Onepage.html">Onepage Docs</a></li>
-                            <li><a href="#">Conversion Tracking</a></li>
-                            <li><a href="cheatsheet.html">Cheatseet</a></li>
+                            <li>Gd. Cibis Nine Lt. 12 Unit G-2</li>
+                            <li>Jl. TB. Simatupang No. 2<br>
+                                Cilandak Timur, Pasar Minggu<br>
+                                Jakarta Selatan 12560</li>
+                            <li>(021) 80623799</li>
+                            <li>mizan.publika@mizan.com</li>
                         </ul>
+                    </div>
+                </div>
+                <div class="col-lg-1 col-sm-6">
+                    <div class="f_widget link_widget pl_70">
+                        <a href="#" class="btn-btn link">Home</a>
+                    </div>
+                </div>
+                <div class="col-lg-1 col-sm-6">
+                    <div class="f_widget link_widget pl_70">
+                        <a href="{{ route('register') }}" class="btn-btn link">Register</a>
+                    </div>
+                </div>
+                <div class="col-lg-1 col-sm-6">
+                    <div class="f_widget link_widget pl_70">
+                        <a href="{{ route('login') }}" class="btn-btn link">Login</a>
                     </div>
                 </div>
             </div>

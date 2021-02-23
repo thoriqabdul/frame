376ac7724692e23e29aa6846483120559dc3e7db
thoriqabdul <thoriqabdul.ta@gmail.com>
2021-02-23 16:08:04 +0700

    seeder

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -11,8 +11,8 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // $this->call(RoleSeed::class);
-        // $this->call(UserSeed::class);
+        $this->call(RoleSeed::class);
+        $this->call(UserSeed::class);
         $this->call(PublisherSeeder::class);
     }
 }
